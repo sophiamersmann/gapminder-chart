07fcb49883e93e9a5107841328c29ac9615723f5
sophiamersmann <sophia.mersmann1@gmail.com>
2022-11-05 16:31:14 +0100

    read data from file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,36 @@
 import Head from 'next/head';
+import { GetStaticProps, NextPage } from 'next';
 
-export default function Home() {
+import { csvParse } from 'd3-dsv';
+
+import { readFileSync } from '../lib/utils';
+import type { DataRow } from '../types/data';
+
+export const getStaticProps: GetStaticProps = async (_context) => {
+  // read data upfront (at build time)
+  const data = csvParse<
+    DataRow,
+    'country' | 'year' | 'gdp_per_capita' | 'life_expectancy' | 'population'
+  >(readFileSync('data/data-by-country-and-year.csv'), (d) => ({
+    country: d.country as string,
+    year: +(d.year as string),
+    gdp: +(d.gdp_per_capita as string),
+    lifeExpectancy: +(d.life_expectancy as string),
+    population: +(d.population as string),
+  }));
+
+  return {
+    props: {
+      data: data,
+    },
+  };
+};
+
+interface IndexProps {
+  data: DataRow[];
+}
+
+const IndexPage: NextPage<IndexProps> = ({ data }: IndexProps) => {
   return (
     <div>
       <Head>
@@ -11,4 +41,6 @@ export default function Home() {
       <main>Hello world!</main>
     </div>
   );
-}
+};
+
+export default IndexPage;
---
+++ types/data.ts
@@ -0,0 +1,7 @@
+export interface DataRow {
+  country: string;
+  year: number;
+  gdp: number;
+  lifeExpectancy: number;
+  population: number;
+}
