227a7d0be8a2f94369a5bccfc9430e11df2ce8e7
sophiamersmann <sophia.mersmann1@gmail.com>
2022-11-13 10:54:02 +0000

    Deploying to gh-pages from @ sophiamersmann/gapminder-chart@20fc45024c48e44aa85a80955f9cbec5061269c8 🚀

--- _next/static/VlQOIuqDcbvMchKMU4arZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/747-08e8a706bf02f24f.js","static/css/0c7987a71a09c610.css","static/chunks/pages/index-78b1911f24866601.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
